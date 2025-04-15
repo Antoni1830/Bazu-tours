38dac38f08956e1f9075c98c1302ef42df0724ed
Antoni1830 <jemmotte08@gmail.com>
2025-04-15 12:05:37 -0400

    lol

--- js/json.js
+++ js/json.js
@@ -1,17 +1,10 @@
-// Función para cargar el archivo JSON
-fetch('hoteles.json')
+const url ='https://antoni1830.github.io/Bazu-tours/hoteles.json'
+fetch(url)
 .then(response => response.json())
-.then(data => {
-    const contenedor = document.getElementById('hoteles');
-    data.forEach(hotel => {
-        const div = document.createElement('div');
-        div.className = 'hotel';
-        div.innerHTML = `
-            <h2>${hotel.nombre}</h2>
-            <p>Ubicación: ${hotel.ubicacion}</p>
-            <img src="${hotel.imagen}" alt="${hotel.nombre}">
-        `;
-        contenedor.appendChild(div);
-    });
-})
-.catch(error => console.error('Error al cargar el JSON:', error));
+.then(data =>{
+
+    let element=document.getElementById('lol2')
+    element.innerHTML=`<img src='${data.imagen}'></img>`
+    console.log(data)
+} )
+.catch(err=>console.log(err))
