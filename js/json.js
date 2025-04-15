eb89264d89706f35bd148b502829921e4dd2da31
Antoni1830 <jemmotte08@gmail.com>
2025-04-15 12:38:07 -0400

    lolo

--- js/json.js
+++ js/json.js
@@ -1,10 +1,8 @@
-const url ='https://antoni1830.github.io/Bazu-tours/hoteles.json'
+const url='https://antoni1830.github.io/wdd230/chamber/rentals.json'
 fetch(url)
 .then(response => response.json())
 .then(data =>{
 
-    let element=document.getElementById('lol2')
-    element.innerHTML=`<img src='${data.imagen}'></img>`
-    console.log(data)
+    
 } )
 .catch(err=>console.log(err))
