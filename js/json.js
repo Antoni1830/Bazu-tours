23a1211307afed20ef1cc22d05f8b037dd97a606
Antoni1830 <jemmotte08@gmail.com>
2025-04-16 11:30:41 -0400

    ok

--- js/json.js
+++ js/json.js
@@ -2,14 +2,14 @@ const url='https://antoni1830.github.io/Bazu-tours/hoteles.json'
 fetch(url)
 .then(response => response.json())
 .then(data =>{
-    let element=document.getElementById('content1')
+    let element=document.getElementById('content9')
     element.innerHTML=`<img src='${data.imagen1}'>
-    <h3>hola</h3>
-    <p>Located in the Piazza del Duomo in Pisa, in the city of the same name, a municipality in the Italian region of Tuscany and capital of the province of the same name in Italy.</p>`
+    <h3>Catedral de Milan</h3>
+    <p>Milan Cathedral is a Gothic cathedral located in the city of the same name.</p>`
 
     let element2=document.getElementById('content2')
     element2.innerHTML=`<img src='${data.imagen2}'>
-    <h3>Coliseo de Roma</h3>
+    <h3>Coliseo de Romaaaaa</h3>
     <p>Located in the Piazza del Duomo in Pisa, in the city of the same name, a municipality in the Italian region of Tuscany and capital of the province of the same name in Italy.</p>`
 
     console.log(data)
