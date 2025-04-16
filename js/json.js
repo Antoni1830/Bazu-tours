338b85f786f64f3e7a29b785d228e65e8fcb2f1e
Antoni1830 <jemmotte08@gmail.com>
2025-04-16 11:51:10 -0400

    ok

--- js/json.js
+++ js/json.js
@@ -3,15 +3,25 @@ fetch(url)
 .then(response => response.json())
 .then(data =>{
     let element=document.getElementById('content9')
-    element.innerHTML=`<img src='${data.imagen1}'>
+    element.innerHTML=`<img src='${data.imagen9}'>
     <h3>Catedral de Milan</h3>
     <p>Milan Cathedral is a Gothic cathedral located in the city of the same name.</p>`
 
     let element2=document.getElementById('content2')
     element2.innerHTML=`<img src='${data.imagen2}'>
-    <h3>Coliseo de Romaaaaa</h3>
-    <p>Located in the Piazza del Duomo in Pisa, in the city of the same name, a municipality in the Italian region of Tuscany and capital of the province of the same name in Italy.</p>`
+    <h3>${data.nombre2}</h3>
+    <p>${data.ubicacion2}</p>`
 
+    let element3=document.getElementById('content3')
+    element2.innerHTML=`<img src='${data.imagen3}'>
+    <h3>${data.nombre3}</h3>
+    <p>${data.ubicacion3}</p>`
+
+    let element4=document.getElementById('content4')
+    element4.innerHTML=`<img src='${data.imagen4}'>
+    <h3>${data.nombre4}</h3>
+    <p>${data.ubicacion4}</p>`
+    
     console.log(data)
     
 } )
